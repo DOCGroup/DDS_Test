4a979a70d180dc87e3e6c99c3bc7f1659865cf70
mxiong <mxiong@b83e41e9-5108-4780-9bb0-1723f710b6d8>
2006-05-26 21:25:09 +0000

    These are the old files which were used during the first tests on TAO DDS.

---
+++ performance/throughput/TAO_DDS/TCP/start_repo1.pl
@@ -0,0 +1,40 @@
+eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
+    & eval 'exec perl -S $0 $argv:q'
+    if 0;
+
+# $Id$
+# -*- perl -*-
+
+use Env (ACE_ROOT);
+use Env (DDS_ROOT);
+use lib "$ACE_ROOT/bin";
+use PerlACE::Run_Test;
+
+$status = 0;
+
+$repo_port = 60001;
+
+$domains_file = PerlACE::LocalFile ("domain_ids");
+$dcpsrepo_ior = PerlACE::LocalFile ("repo.ior");
+
+unlink $dcpsrepo_ior; 
+
+$DCPSREPO = new PerlACE::Process ("$DDS_ROOT/dds/InfoRepo/DCPSInfoRepo",
+                             "-NOBITS -o $dcpsrepo_ior "
+                             . " -ORBDottedDecimalAddresses 1"
+                             . " -ORBEndpoint iiop://blade37.isislab.vanderbilt.edu:$repo_port"
+                             . " -d $domains_file -DCPSConfigFile \"conf.ini\"");
+
+print $DCPSREPO->CommandLine(), "\n";
+
+$DCPSREPO->Spawn ();
+
+if (PerlACE::waitforfile_timed ($dcpsrepo_ior, 30) == -1) {
+    print STDERR "ERROR: waiting for DCPSInfo IOR file\n";
+    $DCPSREPO->Kill ();
+    exit 1;
+}
+
+sleep (99999999999);
+
+exit $status;
---
+++ performance/throughput/TAO_DDS/TCP/start_sub1.pl
@@ -0,0 +1,70 @@
+eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
+    & eval 'exec perl -S $0 $argv:q'
+    if 0;
+
+# $Id$
+# -*- perl -*-
+
+use Env (ACE_ROOT);
+use Env (DDS_ROOT);
+use lib "$ACE_ROOT/bin";
+use PerlACE::Run_Test;
+
+$status = 0;
+
+PerlACE::add_lib_path('../TypeNoKeyBounded');
+
+$primer_messages=100;
+$stats_messages=10000;
+$num_writers=1;
+$repo_port = 60001;
+
+# (possibly allocated by not yet queue by the transport because of greedy read).
+$num_messages=$primer_messages + $stats_messages;
+
+$sub_outfile = PerlACE::LocalFile ("stats_sub");
+
+unlink $sub_outfile;
+
+
+$sub_parameters = "-DCPSConfigFile conf.ini -w $num_writers"
+#                . " -DCPSDebugLevel 6"
+                . " -ORBDottedDecimalAddresses 1"
+                . " -DCPSInfoRepo corbaloc:iiop:blade37.isislab.vanderbilt.edu:$repo_port/DCPSInfoRepo"
+                . " -p $primer_messages -s $stats_messages"
+                . " -r $sub_outfile -msi $num_messages -mxs $num_messages";
+
+
+#use -msi $num_messages to avoid rejected samples
+#use -mxs $num_messages to avoid using the heap 
+#   (could be less than $num_messages but I am not sure of the limit).
+
+@dataSizes =
+  qw'4
+     8
+     16
+     32
+     64
+     128
+     256
+     512
+     1024
+     2048
+     4096
+     8192
+     16384';
+
+foreach $data_size (@dataSizes) {
+  $Subscriber = new PerlACE::Process ("subscriber", $sub_parameters 
+                                                    . " -d $data_size");
+  print $Subscriber->CommandLine(), "\n";
+
+  $SubscriberResult = $Subscriber->SpawnWaitKill (1200);
+
+  if ($SubscriberResult != 0) {
+      print STDERR "ERROR: subscriber returned $SubscriberResult \n";
+      $status = 1;
+  }
+}
+
+exit $status;
