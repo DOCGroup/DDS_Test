d5ec8a51dc175cf854cdf6d7f76d2567fab84768
mxiong <mxiong@b83e41e9-5108-4780-9bb0-1723f710b6d8>
2006-08-08 23:44:23 +0000

    Fixed minor issues with the environment variable usage. -- James

--- scripts/terminate.pl
+++ scripts/terminate.pl
@@ -1,6 +1,12 @@
 #!/usr/bin/perl
 
 
+use Env (DBE_SCRIPTS);
+use Env (DBE_ROOT);
+use Env (DBE_LOCAL_ROOT);
+use Env (DBE_RESULTS_BACKUP);
+
+
 require "$DBE_SCRIPTS/scripts.lib";
 
 # AUTHOR'S NOTE!!!
