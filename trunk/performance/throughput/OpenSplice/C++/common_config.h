692e69359d2dc3d3c692264102701ab3217f279b
parsons <parsons@b83e41e9-5108-4780-9bb0-1723f710b6d8>
2007-02-23 22:00:34 +0000

    ChangeLogTag: Fri Feb 23 21:50:21 UTC 2007  Jeff Parsons <j.parsons@vanderbilt.edu>

--- trunk/performance/throughput/OpenSplice/C++/common_config.h
+++ trunk/performance/throughput/OpenSplice/C++/common_config.h
@@ -4,37 +4,51 @@
 #ifndef __COMMON_CONFIG_H
 #define __COMMON_CONFIG_H
 
-//#include "Stats.h"
+// To avoid a clash of typedefs in ACE and OpenSplice
+#define pid_t ACE_pid_t_typedef_clash_workaround
+#define mode_t ACE_mode_t_typedef_clash_workaround
+
 #include "ace/Log_Msg.h"
-#include "ace/CDR_Base.h"
+#include "ace/Arg_Shifter.h"
 #include "ace/Sched_Params.h"
+#include "Stats.h"
 
+#undef pid_t
+#undef mode_t
+
+// To avoid a redef error on Windows from OpenSplice header files,
+// ACE defines it in config-win32-common.h.
+#if defined (FD_SETSIZE)
+#undef FD_SETSIZE
+#endif
+
+#include "TPTDefDcps_impl.h"
 #include "ccpp_dds_dcps.h"
-#include "ccpp_TPTDef.h"
 
-using namespace DDS;
-using namespace TP_Test;
+const char *pub_output_file = "stats_pub";
+const char *sub_output_file = "stats_sub";
+const char *network_config_file = "net.txt";
+const char *qos_config_file = "qos.txt";
+const char *test_topic_name = "TEST_TOPIC";
+const char *test_type_name = "TP_Test::Bytes";
+const char *write_partition = "TEST";
+const char *read_partition  = "TEST";
 
-const ACE_CDR::Char *pub_output_file ("stats_pub");
-const ACE_CDR::Char *sub_output_file ("stats_sub");
-const ACE_CDR::Char *network_config_file ("net.txt");
-const ACE_CDR::Char *qos_config_file ("qos.txt");
-const ACE_CDR::Char *test_topic_name ("TEST_TOPIC");
-const ACE_CDR::Char *test_type_name = ("");
-
-ACE_CDR::ULong PRIMER_SAMPLES = 0;
-ACE_CDR::ULong STATS_SAMPLES = 100;
-ACE_CDR::ULong DATA_SIZE = 1;
+unsigned long PRIMER_SAMPLES = 0;
+unsigned long STATS_SAMPLES = 100;
+unsigned long DATA_SIZE = 1;
 
 int num_datawriters = 1;
 int num_datareaders = 1;
 
+// mydebug
+bool transfer_complete = false;
+int counter = 0;
 
 // This can be changed to the desired value.
 const int PRIORITY =
   (ACE_Sched_Params::priority_min (ACE_SCHED_FIFO)
    + ACE_Sched_Params::priority_max (ACE_SCHED_FIFO)) / 2;
-
 
 // Attempt to set the real time priority and lock memory.
 void
@@ -60,170 +74,5 @@ set_rt (void)
         }
     }
 }
-
-
-// Template function that wraps around the DataWriter's write ()
-// function to have it send the data and also use the data for stats.
-// Send data using this writewstats () function instead of write ().
-// writewstats() timestamps the samples being sent and record the timestamps 
-// to a Stats object.
-// It shuts down its timer and dumps the stats to file once the
-// transfer is complete.
-template <typename FooDataType, typename FooDataWriter_ptr>
-ReturnCode_t 
-writewstats (DataWriter_ptr dw/*, PubSub_Stats &mystats*/)
-{
-  ReturnCode_t retcode;
-
-  FooDataType sample;
-  sample.timestamp = 0;
-
-  FooDataWriter_ptr typed_dw;
-  typed_dw = dynamic_cast<FooDataWriter_ptr> (dw);
-
-  for (int i = 0; i < 5 * (PRIMER_SAMPLES + STATS_SAMPLES); i++) {
-    retcode = typed_dw->write (sample, HANDLE_NIL);
-  }
-
-  return retcode;
-}
-
-
-// Template function that wraps around the DataReader's take ()
-// function to have it take the data and also use the data for stats.
-// Take data using this takewstats () function instead of take ().
-// takewstats() examines the samples received, extracts out the timestamps
-// and record them to a Stats object.
-// It shuts down its timer and dumps the stats to file once the
-// transfer is complete.
-template <typename FooDataSequence, typename FooDataSequence_var, typename FooDataReader_ptr>
-ReturnCode_t
-takewstats (DataReader_ptr dr/*, PubSub_Stats &mystats*/)
-{
-  ReturnCode_t retcode;
-
-  FooDataSequence_var received_seq = new FooDataSequence;
-  SampleInfoSeq_var info_seq = new SampleInfoSeq;
-
-  FooDataReader_ptr typed_dr;
-  typed_dr = dynamic_cast<FooDataReader_ptr> (dr);
-
-  retcode = typed_dr->take (received_seq,
-                            info_seq,
-		            1,
-			    ANY_SAMPLE_STATE,
-		            ANY_VIEW_STATE,
-		            ANY_INSTANCE_STATE);
-
-  
-  printf ("Sub: Taking one sample.\n");
-
-  return retcode;
-}
-
-
-ReturnCode_t typed_write (DataWriter_ptr dw, ACE_CDR::ULong data_size)
-{
-  ReturnCode_t retcode;
-
-  switch (data_size) {
-    case 4:
-      retcode = writewstats<Bytes4, Bytes4DataWriter_ptr> (dw);
-      break;
-    case 8:
-      retcode = writewstats<Bytes8, Bytes8DataWriter_ptr> (dw);
-      break;
-    case 16:
-      retcode = writewstats<Bytes16, Bytes16DataWriter_ptr> (dw);
-      break;
-    case 32:
-      retcode = writewstats<Bytes32, Bytes32DataWriter_ptr> (dw);
-      break;
-    case 64:
-      retcode = writewstats<Bytes64, Bytes64DataWriter_ptr> (dw);
-      break;
-    case 128:
-      retcode = writewstats<Bytes128, Bytes128DataWriter_ptr> (dw);
-      break;
-    case 256:
-      retcode = writewstats<Bytes256, Bytes256DataWriter_ptr> (dw);
-      break;
-    case 512:
-      retcode = writewstats<Bytes512, Bytes512DataWriter_ptr> (dw);
-      break;
-    case 1024:
-      retcode = writewstats<Bytes1024, Bytes1024DataWriter_ptr> (dw);
-      break;
-    case 2048:
-      retcode = writewstats<Bytes2048, Bytes2048DataWriter_ptr> (dw);
-      break;
-    case 4096:
-      retcode = writewstats<Bytes4096, Bytes4096DataWriter_ptr> (dw);
-      break;
-    case 8192:
-      retcode = writewstats<Bytes8192, Bytes8192DataWriter_ptr> (dw);
-      break;
-    case 16384:
-      retcode = writewstats<Bytes16384, Bytes16384DataWriter_ptr> (dw);
-      break;
-    default:
-      printf ("Sub: DATA_SIZE not supported!\n");
-  }
-
-  return retcode;
-}
-
-
-ReturnCode_t typed_take (DataReader_ptr dr, ACE_CDR::ULong data_size)
-{
-  ReturnCode_t retcode;
-
-  switch (data_size) {
-    case 4:
-      retcode = takewstats<Bytes4Seq, Bytes4Seq_var, Bytes4DataReader_ptr> (dr);
-      break;
-    case 8:
-      retcode = takewstats<Bytes8Seq, Bytes8Seq_var, Bytes8DataReader_ptr> (dr);
-      break;
-    case 16:
-      retcode = takewstats<Bytes16Seq, Bytes16Seq_var, Bytes16DataReader_ptr> (dr);
-      break;
-    case 32:
-      retcode = takewstats<Bytes32Seq, Bytes32Seq_var, Bytes32DataReader_ptr> (dr);
-      break;
-    case 64:
-      retcode = takewstats<Bytes64Seq, Bytes64Seq_var, Bytes64DataReader_ptr> (dr);
-      break;
-    case 128:
-      retcode = takewstats<Bytes128Seq, Bytes128Seq_var, Bytes128DataReader_ptr> (dr);
-      break;
-    case 256:
-      retcode = takewstats<Bytes256Seq, Bytes256Seq_var, Bytes256DataReader_ptr> (dr);
-      break;
-    case 512:
-      retcode = takewstats<Bytes512Seq, Bytes512Seq_var, Bytes512DataReader_ptr> (dr);
-      break;
-    case 1024:
-      retcode = takewstats<Bytes1024Seq, Bytes1024Seq_var, Bytes1024DataReader_ptr> (dr);
-      break;
-    case 2048:
-      retcode = takewstats<Bytes2048Seq, Bytes2048Seq_var, Bytes2048DataReader_ptr> (dr);
-      break;
-    case 4096:
-      retcode = takewstats<Bytes4096Seq, Bytes4096Seq_var, Bytes4096DataReader_ptr> (dr);
-      break;
-    case 8192:
-      retcode = takewstats<Bytes8192Seq, Bytes8192Seq_var, Bytes8192DataReader_ptr> (dr);
-      break;
-    case 16384:
-      retcode = takewstats<Bytes16384Seq, Bytes16384Seq_var, Bytes16384DataReader_ptr> (dr);
-      break;
-    default:
-      printf ("Sub: DATA_SIZE not supported!\n");
-  }
-
-  return retcode;
-}
-
 
 #endif /* __COMMON_CONFIG_H */
