f36266b627248245fbef9ee0b59fc087030c0746
parsons <j.parsons@vanderbilt.edu>
2007-02-23 22:00:34 +0000

    ChangeLogTag: Fri Feb 23 21:50:21 UTC 2007  Jeff Parsons <j.parsons@vanderbilt.edu>

--- performance/throughput/OpenSplice/C++/publisher.cpp
+++ performance/throughput/OpenSplice/C++/publisher.cpp
@@ -1,50 +1,153 @@
 #ifndef __PUBLISHER_CPP
 #define __PUBLISHER_CPP
 
-#include "Stats.h"
-#include "ace/Log_Msg.h"
-
-#include "ccpp_dds_dcps.h"
-#include "ccpp_TPTDef.h"
-
-#include <iostream>
-#include "getopt.h"
 #include "common_config.h"
-#include "types_registration.h"
-
-using namespace std;
-using namespace DDS;
-using namespace CORBA;
-using namespace TP_Test;
-
-static char * write_partition = "TEST";
-static char * read_partition  = "TEST";
-
 
 int
-main (
-    int argc,
-    char *argv[]
-    )
+parse_pub_args (int argc, char *argv[])
 {
-  DomainId_t                   	 myDomain           = NULL; 
-  DomainParticipantFactory_ptr 	 dpf                = NULL; 
-  DomainParticipant_ptr        	 dp                 = NULL;
-  Publisher_ptr                	 p                  = NULL;
-
-  DataWriter_ptr               	 dw                 = NULL;  
-  Topic_ptr                      test_topic         = NULL;
+  ACE_Arg_Shifter arg_shifter (argc, argv);
   
+  arg_shifter.ignore_arg (); // ignore the command - argv[0]
+  
+  while (arg_shifter.is_anything_left ()) 
+  {
+    // options:
+    // -w  <num data writers>
+    // -d  <data size>
+    // -p <num primer samples>
+    // -s <num stats samples>
+    // -r <stats output file>
+    // -top <test's topic name>
+    // -n <network settings file>
+    // -q <QoS policies file>
+    // -msi <max samples per instance>
+    // -mxs <max samples>
+    // -mxi <max instances>
+
+    const char *currentArg = 0;
     
-  DomainParticipantQos                  dpQos;
-  TopicQos                              tQos;    
-  PublisherQos                          pQos;
-  DataWriterQos                         dwQos;
+    if ((currentArg = arg_shifter.get_the_parameter ("-writers")) != 0) 
+      {
+        num_datawriters = ACE_OS::atoi (currentArg);
+        arg_shifter.consume_arg ();
+      }
+    else if ((currentArg = arg_shifter.get_the_parameter ("-d")) != 0) 
+      {
+        DATA_SIZE = ACE_OS::atoi (currentArg);
+        arg_shifter.consume_arg ();
+      }
+    else if ((currentArg = arg_shifter.get_the_parameter ("-p")) != 0) 
+      {
+        PRIMER_SAMPLES = ACE_OS::atoi (currentArg);
+        arg_shifter.consume_arg ();
+      }
+    else if ((currentArg = arg_shifter.get_the_parameter ("-s")) != 0) 
+      {
+        STATS_SAMPLES = ACE_OS::atoi (currentArg);
+        arg_shifter.consume_arg ();
+      }
+    else if ((currentArg = arg_shifter.get_the_parameter ("-r")) != 0)
+      {
+        pub_output_file = currentArg;
+        arg_shifter.consume_arg ();
+      }
 
-  ReturnCode_t                         retcode;
+    else if ((currentArg = arg_shifter.get_the_parameter ("-top")) != 0)
+      {
+        test_topic_name = currentArg;
+        arg_shifter.consume_arg ();
+      }
+    else if ((currentArg = arg_shifter.get_the_parameter ("-n")) != 0)
+      {
+        network_config_file = currentArg;
+        arg_shifter.consume_arg ();
+      }
+    else if ((currentArg = arg_shifter.get_the_parameter ("-q")) != 0)
+      {
+        qos_config_file = currentArg;
+        arg_shifter.consume_arg ();
+      }
+    //else if ((currentArg = arg_shifter.get_the_parameter ("-msi")) != 0) 
+    //  {
+    //    MAX_SAMPLES_PER_INSTANCE = ACE_OS::atoi (currentArg);
+    //    arg_shifter.consume_arg ();
+    //  }
+    //else if ((currentArg = arg_shifter.get_the_parameter ("-mxs")) != 0)
+    //  {
+    //    MAX_SAMPLES = ACE_OS::atoi (currentArg);
+    //    arg_shifter.consume_arg ();
+    //  }
+    //else if ((currentArg = arg_shifter.get_the_parameter ("-mxi")) != 0)
+    //  {
+    //    MAX_INSTANCES = ACE_OS::atoi (currentArg);
+    //    arg_shifter.consume_arg ();
+    //  }
+    //else if ((currentArg = arg_shifter.get_the_parameter ("-sd")) != 0) 
+    //  {
+    //    subscriber_delay_msec = ACE_OS::atoi (currentArg);
+    //    arg_shifter.consume_arg ();
+    //  }
+    else 
+      {
+        ACE_ERROR ((LM_ERROR,
+                    "(%P|%t) unexpected parameter %s\n",
+                    arg_shifter.get_current ()));
+        arg_shifter.ignore_arg ();
+        return 3;
+      }
+  }
+  
+  // Indicates sucessful parsing of the command line.
+  return 0;
+}
 
+// Template function that wraps around the DataWriter's write ()
+// function to have it send the data and also use the data for stats.
+// Send data using this writewstats () function instead of write ().
+// writewstats() timestamps the samples being sent and record the timestamps 
+// to a Stats object.
+// It shuts down its timer and dumps the stats to file once the
+// transfer is complete.
+template <typename FooDataType, typename FooDataWriter_ptr>
+DDS::ReturnCode_t 
+writewstats (DDS::DataWriter_ptr dw/*, PubSub_Stats &mystats*/)
+{
+  DDS::ReturnCode_t retcode;
 
+  FooDataType sample;
+  sample.timestamp = 0;
 
+  FooDataWriter_ptr typed_dw;
+  typed_dw = dynamic_cast<FooDataWriter_ptr> (dw);
+
+  for (unsigned long i = 0;
+       i < 5 * (PRIMER_SAMPLES + STATS_SAMPLES);
+       ++i)
+    {
+      retcode = typed_dw->write (sample, DDS::HANDLE_NIL);
+    }
+
+  return retcode;
+}
+
+int
+main (int argc, char *argv[])
+{
+  DDS::DomainId_t                   	 myDomain           = NULL; 
+  DDS::DomainParticipantFactory_ptr 	 dpf                = NULL; 
+  DDS::DomainParticipant_ptr        	 dp                 = NULL;
+  DDS::Publisher_ptr                	 p                  = NULL;
+
+  DDS::DataWriter_ptr               	 dw                 = NULL;  
+  DDS::Topic_ptr                       test_topic         = NULL;
+     
+  DDS::DomainParticipantQos                  dpQos;
+  DDS::TopicQos                              tQos;    
+  DDS::PublisherQos                          pQos;
+  DDS::DataWriterQos                         dwQos;
+
+  DDS::ReturnCode_t                         retcode;
 
   // parsing arguments options:
   // -w  <num data readers>
@@ -59,65 +162,56 @@ main (
   // -q <QoS policies file>
   int parsing_status = parse_pub_args (argc, argv);
 
-  
-
-  printf ("Listener-based...\n");
-
-    // Initialize stats object
+  // Initialize stats object
   PubSub_Stats *pubstats = new PubSub_Stats (pub_output_file,
                                              PRIMER_SAMPLES,
                                              STATS_SAMPLES,
                                              DATA_SIZE);
-
-
-    
+                                               
   //
   // Create participant
   //
-  dpf = DomainParticipantFactory::get_instance ();
+  dpf = DDS::DomainParticipantFactory::get_instance ();
   dp = dpf->create_participant (myDomain, PARTICIPANT_QOS_DEFAULT, NULL);
-  if (dp == NULL) {
-      cout << "Pub: ERROR - Splice Daemon not running";
-      exit (1);
-  }
 
+  if (dp == NULL)
+    {
+      ACE_ERROR_RETURN ((LM_ERROR,
+                          "Pub: ERROR - Splice Daemon not running"),
+                        1);
+    }
 
   // 
   // Create PING publisher
   //
   dp->get_default_publisher_qos (pQos);
   pQos.partition.name.length (1);
-  pQos.partition.name[0] = string_dup (write_partition);
+  pQos.partition.name[0] = DDS::string_dup (write_partition);
   p = dp->create_publisher (pQos, NULL);
 
-
   // Register type
-  TP_Test_register_type (dp, DATA_SIZE, &test_type_name);
-
-  cout << "Pub: Test type name is " << test_type_name << endl;
-
+  TP_Test::BytesTypeSupport Bytes_dt;
+  Bytes_dt.register_type (dp, test_type_name);
 
   //  Create Topic
-  test_topic = dp->create_topic (test_topic_name, test_type_name, TOPIC_QOS_DEFAULT, NULL);
+  test_topic = dp->create_topic (test_topic_name,
+                                 test_type_name,
+                                 TOPIC_QOS_DEFAULT,
+                                 NULL);
 
   // Create datawriter
   dw = p->create_datawriter (test_topic, DATAWRITER_QOS_DEFAULT, NULL);
-  
-  
 
   // Send data
-  typed_write (dw, DATA_SIZE);
-
-
+  retcode =
+    writewstats<TP_Test::Bytes, TP_Test::BytesDataWriter_ptr> (dw);
 
   retcode = p->delete_datawriter (dw);
   retcode = dp->delete_publisher (p);
   retcode = dp->delete_topic (test_topic);
   retcode = dpf->delete_participant (dp);
 
-  //delete pubstats;
-  printf ("Pub: Shut down...\n");
-
+  delete pubstats;
   return 0;
 }
 
--- performance/throughput/OpenSplice/C++/subscriber.cpp
+++ performance/throughput/OpenSplice/C++/subscriber.cpp
@@ -1,93 +1,203 @@
 #ifndef __SUBSCRIBER_CPP
 #define __SUBSCRIBER_CPP
 
-//#include "Stats.h"
-#include "ace/Log_Msg.h"
-
-#include "ccpp_dds_dcps.h"
-#include "ccpp_TPTDef.h"
-
-#include <iostream>
-#include "getopt.h"
 #include "common_config.h"
-#include "types_registration.h"
 
-using namespace std;
-using namespace DDS;
-using namespace CORBA;
-using namespace TP_Test;
-
-static char * write_partition = "TEST";
-static char * read_partition  = "TEST";
-
-// mydebug
-bool transfer_complete = false;
-int counter = 0;
-
-class MyDataReaderListener : public DataReaderListener
+int
+parse_sub_args (int argc, char *argv[])
 {
-  void on_requested_deadline_missed (DataReader_ptr reader,
-                                     const RequestedDeadlineMissedStatus& status) throw (SystemException) 
+  ACE_Arg_Shifter arg_shifter (argc, argv);
+  
+  arg_shifter.ignore_arg (); // ignore the command - argv[0]
+  
+  while (arg_shifter.is_anything_left ()) 
+  {
+    // options:
+    // -w  <num data readers>
+    // -d  <data size>
+    // -p <num primer samples>
+    // -s <num stats samples>
+    // -r <stats output file>
+    // -top <test's topic name>
+    // -n <network settings file>
+    // -q <QoS policies file>
+    // -msi <max samples per instance>
+    // -mxs <max samples>
+    // -mxi <max instances>
+
+    const char *currentArg = 0;
+    
+    if ((currentArg = arg_shifter.get_the_parameter ("-readers")) != 0) 
+      {
+        num_datareaders = ACE_OS::atoi (currentArg);
+        arg_shifter.consume_arg ();
+      }
+    else if ((currentArg = arg_shifter.get_the_parameter ("-d")) != 0) 
+      {
+        DATA_SIZE = ACE_OS::atoi (currentArg);
+        arg_shifter.consume_arg ();
+      }
+    else if ((currentArg = arg_shifter.get_the_parameter ("-p")) != 0) 
+      {
+        PRIMER_SAMPLES = ACE_OS::atoi (currentArg);
+        arg_shifter.consume_arg ();
+      }
+    else if ((currentArg = arg_shifter.get_the_parameter ("-s")) != 0) 
+      {
+        STATS_SAMPLES = ACE_OS::atoi (currentArg);
+        arg_shifter.consume_arg ();
+      }
+    else if ((currentArg = arg_shifter.get_the_parameter ("-r")) != 0)
+      {
+        sub_output_file = currentArg;
+        arg_shifter.consume_arg ();
+      }
+
+    else if ((currentArg = arg_shifter.get_the_parameter ("-top")) != 0)
+      {
+        test_topic_name = currentArg;
+        arg_shifter.consume_arg ();
+      }
+    else if ((currentArg = arg_shifter.get_the_parameter ("-n")) != 0)
+      {
+        network_config_file = currentArg;
+        arg_shifter.consume_arg ();
+      }
+    else if ((currentArg = arg_shifter.get_the_parameter ("-q")) != 0)
+      {
+        qos_config_file = currentArg;
+        arg_shifter.consume_arg ();
+      }
+    //else if ((currentArg = arg_shifter.get_the_parameter ("-msi")) != 0) 
+    //  {
+    //    MAX_SAMPLES_PER_INSTANCE = ACE_OS::atoi (currentArg);
+    //    arg_shifter.consume_arg ();
+    //  }
+    //else if ((currentArg = arg_shifter.get_the_parameter ("-mxs")) != 0)
+    //  {
+    //    MAX_SAMPLES = ACE_OS::atoi (currentArg);
+    //    arg_shifter.consume_arg ();
+    //  }
+    //else if ((currentArg = arg_shifter.get_the_parameter ("-mxi")) != 0)
+    //  {
+    //    MAX_INSTANCES = ACE_OS::atoi (currentArg);
+    //    arg_shifter.consume_arg ();
+    //  }
+    //else if ((currentArg = arg_shifter.get_the_parameter ("-sd")) != 0) 
+    //  {
+    //    subscriber_delay_msec = ACE_OS::atoi (currentArg);
+    //    arg_shifter.consume_arg ();
+    //  }
+    else 
+      {
+        ACE_ERROR ((LM_ERROR,
+                    "(%P|%t) unexpected parameter %s\n",
+                    arg_shifter.get_current ()));
+        arg_shifter.ignore_arg ();
+        return 3;
+      }
+  }
+  
+  // Indicates sucessful parsing of the command line.
+  return 0;
+}
+
+// Template function that wraps around the DataReader's take ()
+// function to have it take the data and also use the data for stats.
+// Take data using this takewstats () function instead of take ().
+// takewstats() examines the samples received, extracts out the timestamps
+// and record them to a Stats object.
+// It shuts down its timer and dumps the stats to file once the
+// transfer is complete.
+template <typename FooDataSequence,
+          typename FooDataSequence_var,
+          typename FooDataReader_ptr>
+DDS::ReturnCode_t
+takewstats (DDS::DataReader_ptr dr/*, PubSub_Stats &mystats*/)
+{
+  FooDataSequence_var received_seq = new FooDataSequence;
+  DDS::SampleInfoSeq_var info_seq = new DDS::SampleInfoSeq;
+  FooDataReader_ptr typed_dr = dynamic_cast<FooDataReader_ptr> (dr);
+
+  DDS::ReturnCode_t retcode =
+    typed_dr->take (received_seq,
+                    info_seq,
+		                1,
+			              DDS::ANY_SAMPLE_STATE,
+		                DDS::ANY_VIEW_STATE,
+		                DDS::ANY_INSTANCE_STATE);
+
+  return retcode;
+}
+
+class MyDataReaderListener : public DDS::DataReaderListener
+{
+  void on_requested_deadline_missed (DDS::DataReader_ptr reader,
+                                     const DDS::RequestedDeadlineMissedStatus& status)
+    throw (CORBA::SystemException) 
   {}
 
-  void on_requested_incompatible_qos (DataReader_ptr reader,
-                                      const RequestedIncompatibleQosStatus& status) throw (SystemException)
+  void on_requested_incompatible_qos (DDS::DataReader_ptr reader,
+                                      const DDS::RequestedIncompatibleQosStatus& status)
+    throw (CORBA::SystemException)
   {}
 
-  void on_sample_rejected (DataReader_ptr reader,
-                           const SampleRejectedStatus& status) throw (SystemException)
+  void on_sample_rejected (DDS::DataReader_ptr reader,
+                           const DDS::SampleRejectedStatus& status)
+    throw (CORBA::SystemException)
   {}
 
-  void on_liveliness_changed (DataReader_ptr reader,
-                              const LivelinessChangedStatus& status) throw (SystemException)
+  void on_liveliness_changed (DDS::DataReader_ptr reader,
+                              const DDS::LivelinessChangedStatus& status)
+    throw (CORBA::SystemException)
   {}
 
-  void on_data_available (DataReader_ptr reader) throw (SystemException)
+  void on_data_available (DDS::DataReader_ptr dr)
+    throw (CORBA::SystemException)
   {
     counter++;
+    
     if (counter > 10)
-      transfer_complete = true;
-    typed_take (reader, DATA_SIZE);
+      {
+        transfer_complete = true;
+      }
+      
+    DDS::ReturnCode_t retcode =
+      takewstats<TP_Test::BytesSeq,
+                 TP_Test::BytesSeq_var,
+                 TP_Test::BytesDataReader_ptr> (dr);
   }
 
-  void on_subscription_match (DataReader_ptr reader,
-                              const SubscriptionMatchStatus& status) throw (SystemException)
+  void on_subscription_match (DDS::DataReader_ptr reader,
+                              const DDS::SubscriptionMatchStatus& status)
+    throw (CORBA::SystemException)
   {}
 
-  void on_sample_lost (DataReader_ptr reader,
-                       const SampleLostStatus& status) throw (SystemException)
+  void on_sample_lost (DDS::DataReader_ptr reader,
+                       const DDS::SampleLostStatus& status)
+    throw (CORBA::SystemException)
   {}  
 };
 
-
 int
-main (
-    int argc,
-    char *argv[]
-    )
+main (int argc, char *argv[])
 {
-  DomainId_t                        myDomain           = NULL;
-  DomainParticipantFactory_ptr      dpf                = NULL;
-  DomainParticipant_ptr             dp                 = NULL;
-  Subscriber_ptr                    s                  = NULL;
+  DDS::DomainId_t                        myDomain           = NULL;
+  DDS::DomainParticipantFactory_ptr      dpf                = NULL;
+  DDS::DomainParticipant_ptr             dp                 = NULL;
+  DDS::Subscriber_ptr                    s                  = NULL;
     
-  DataReader_ptr                    dr                 = NULL;
+  DDS::DataReader_ptr                    dr                 = NULL;
   
-  Topic_ptr                         test_topic         = NULL;
+  DDS::Topic_ptr                         test_topic         = NULL;
 
+  DDS::DomainParticipantQos              dpQos;
+  DDS::TopicQos                          tQos;
+       
+  DDS::SubscriberQos                     sQos;
+  DDS::DataReaderQos                     drQos;
 
-  DomainParticipantQos              dpQos;
-  TopicQos                          tQos;
-    
-    
-  SubscriberQos                     sQos;
-  DataReaderQos                     drQos;
-
-  ReturnCode_t                      retcode;
-
-
-
-
+  DDS::ReturnCode_t                      retcode;
 
   // parsing arguments options:
   // -w  <num data readers>
@@ -102,64 +212,59 @@ main (
   // -q <QoS policies file>
   int parsing_status = parse_sub_args (argc, argv);
 
-  cout << "Data size is " << DATA_SIZE << endl;
-
-
-
-  printf ("Listener-based...\n");
-
   // Initialize stats object
-  //PubSub_Stats *substats = new PubSub_Stats (sub_output_file,
-  //                                           PRIMER_SAMPLES,
-		//			     STATS_SAMPLES,
-		//			     DATA_SIZE);
-
+  PubSub_Stats *substats = new PubSub_Stats (sub_output_file,
+                                             PRIMER_SAMPLES,
+					                                   STATS_SAMPLES,
+					                                   DATA_SIZE);
 
   //
   // Create participant
   //
-
-  dpf = DomainParticipantFactory::get_instance ();
+  dpf = DDS::DomainParticipantFactory::get_instance ();
   dp = dpf->create_participant (myDomain, PARTICIPANT_QOS_DEFAULT, NULL);
-  if (dp == NULL) {
-      cout << "Sub: ERROR - Splice Daemon not running";
-      exit(1);
-  }
-
-
+  
+  if (dp == NULL)
+    {
+      ACE_ERROR_RETURN ((LM_ERROR,
+                         "Sub: ERROR - Splice Daemon not running"),
+                        1);
+    }
 
   //
   // Create subscriber
   //
-
   dp->get_default_subscriber_qos (sQos);
   sQos.partition.name.length (1);
-  sQos.partition.name[0] = string_dup (read_partition);
+  sQos.partition.name[0] = DDS::string_dup (read_partition);
   s = dp->create_subscriber (sQos, NULL);
-
-
-      
+     
   // Register type
-  TP_Test_register_type (dp, DATA_SIZE, &test_type_name);
-  cout << "Sub: Test type name is " << test_type_name << endl;
+  TP_Test::BytesTypeSupport Bytes_dt;
+  Bytes_dt.register_type (dp, test_type_name);
   
   //  Create Topic
-  test_topic = dp->create_topic (test_topic_name, test_type_name, TOPIC_QOS_DEFAULT, NULL);
-
+  test_topic = dp->create_topic (test_topic_name,
+                                 test_type_name,
+                                 TOPIC_QOS_DEFAULT,
+                                 NULL);
 
   // Create datareader
   MyDataReaderListener* mylistener = new MyDataReaderListener;
   dr = s->create_datareader (test_topic, DATAREADER_QOS_DEFAULT, NULL);
-  printf ("Sub: Create reader successful.\n");
 
   // Attach listener to datareader
-  retcode = dr->set_listener (mylistener, DATA_AVAILABLE_STATUS);
-  if (retcode == RETCODE_OK)
-    printf ("Sub: Attach listener successful.\n");
+  retcode = dr->set_listener (mylistener, DDS::DATA_AVAILABLE_STATUS);
+  
+  if (retcode != DDS::RETCODE_OK)
+    {
+      ACE_ERROR_RETURN ((LM_ERROR,
+                         "Sub: Attach listener unsuccessful.\n"),
+                        1);
+    }
 
 
   while (!transfer_complete) {}
-
 
   retcode = s->delete_datareader (dr);  
   retcode = dp->delete_subscriber (s);
@@ -167,12 +272,7 @@ main (
   retcode = dpf->delete_participant (dp);
 
   delete mylistener;
-
-
-
-
-  //delete substats;
-  printf ("Sub: Shut down...\n");
+  delete substats;
 
   return 0;
 }
