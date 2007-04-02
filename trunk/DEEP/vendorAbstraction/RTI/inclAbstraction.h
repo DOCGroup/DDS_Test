0be4dd66729f6b415830c9b89f4a8b8c91033c11
parsons <parsons@b83e41e9-5108-4780-9bb0-1723f710b6d8>
2007-04-02 21:05:22 +0000

    ChangeLogTag: Mon Apr  2 20:54:33 UTC 2007  Jeff Parsons <j.parsons@vanderbilt.edu>

--- trunk/DEEP/vendorAbstraction/RTI/inclAbstraction.h
+++ trunk/DEEP/vendorAbstraction/RTI/inclAbstraction.h
@@ -1,4 +1,35 @@
 #ifndef INCLABSTRACTION_H_
 #define INCLABSTRACTION_H_
 
+/* NDDS header file */
+//#include "ndds/ndds_cpp.h"
+
+/* Type header files */
+#include "deepTypesSupport.h"
+//#include "deepTypes.h"
+
+#include <iostream>
+
+using namespace std;
+
+// For better compliance with OMG IDL PSM C++ mapping.
+namespace DDS
+{
+  typedef DDSDomainParticipantFactory DomainParticipantFactory;
+  
+  typedef DDSDomainParticipant DomainParticipant;
+  typedef DomainParticipant *DomainParticipant_ptr;
+  typedef DDS_DomainParticipantQos DomainParticipantQos;
+  typedef DDSDomainParticipantListener DomainParticipantListener;
+  
+  typedef DDSTopic Topic;
+  typedef DDS_TopicQos TopicQos;
+  typedef DDSTopicListener TopicListener;
+  
+  typedef DDS_StatusMask StatusMask;
+  
+  typedef DDS_ReturnCode_t ReturnCode_t;
+  typedef DDS_Duration_t Duration_t;
+}
+
 #endif /*INCLABSTRACTION_H_*/
--- trunk/DEEP/vendorAbstraction/TAO_DDS/inclAbstraction.h
+++ trunk/DEEP/vendorAbstraction/TAO_DDS/inclAbstraction.h
@@ -1,4 +1,52 @@
 #ifndef INCLABSTRACTION_H_
 #define INCLABSTRACTION_H_
 
+typedef long pid_t;
+typedef int mode_t;
+
+#include "ace/OS_NS_sys_time.h"
+
+#include "dds/DCPS/Qos_Helper.h"
+#include "dds/DCPS/Service_Participant.h"
+#include "dds/DCPS/Marked_Default_Qos.h"
+
+#include "simpleTypeTypeSupportImpl.h"
+#include "nestedTypeTypeSupportImpl.h"
+#include "arrayTypeTypeSupportImpl.h"
+#include "stringTypeTypeSupportImpl.h"
+
+#include <iostream>
+
+using namespace std;
+
+#define DEEP_INSTANCE_HANDLE_NIL DDS::HANDLE_NIL
+
+// Temporary hacks to compile until framework is changed to use listeners.
+namespace DDS
+{
+  class WaitSet
+  {
+  };
+  
+  typedef WaitSet *WaitSet_ptr;
+}
+
+inline DDS::DomainParticipant_ptr DEEP_create_participant(const char *domainId) {
+    const char *domainIdParm;
+    ::DDS::DomainParticipantFactory_var dpf = TheParticipantFactory;
+
+    if ((domainId != NULL) && (*domainId == '\0'))
+      {
+        domainIdParm = NULL;
+      } 
+    else
+      {
+        domainIdParm = domainId;
+      }
+
+    ACE_CString conv (domainId);
+    DDS::DomainId_t did = static_cast<CORBA::Long> (conv.hash ());
+    return dpf->create_participant (did, PARTICIPANT_QOS_DEFAULT, NULL);
+}
+
 #endif /*INCLABSTRACTION_H_*/
