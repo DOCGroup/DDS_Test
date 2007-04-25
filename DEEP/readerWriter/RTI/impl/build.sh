c0ef996a664225fac6d55dd04ad8227d3f46ab61
mxiong <mxiong@b83e41e9-5108-4780-9bb0-1723f710b6d8>
2007-04-25 18:06:32 +0000

    removed the old vendor specific folders

--- DEEP/readerWriter/RTI/impl/build.sh
+++
@@ -1,3 +0,0 @@
-echo "Building $1 with g++"
-g++ -g -m32 -DRTI_UNIX -DRTI_LINUX  -I../.. -I$DEEP_HOME/scenario -I$DEEP_HOME/readerWriter/RTI/generated -I$DEEP_HOME/vendorAbstraction/RTI -I$NDDSHOME/include -I$NDDSHOME/include/ndds  -c $1.cpp -o ../../../obj/RTI/$1.o
-
