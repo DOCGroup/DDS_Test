c3f10e5d6e4bd98677bcdd805a4d992bbcd8e274
mxiong <mxiong@b83e41e9-5108-4780-9bb0-1723f710b6d8>
2007-04-25 18:06:32 +0000

    removed the old vendor specific folders

--- trunk/DEEP/readerWriter/OpenSplice/generated/build_TypeSupport.sh
+++
@@ -1,2 +0,0 @@
-echo "Compiling IDL with Splice IDL compiler"
-idlpp -I../../ -I$OSPL_HOME/include/dcps/C++/SACPP -l cpp -S ../../deepTypes.idl
--- trunk/DEEP/readerWriter/OpenSplice/impl/build.sh
+++
@@ -1,2 +0,0 @@
-echo "Building $1 with g++3.2.3"
-g++3.2.3 -g -DDDS_OpenFusion_1_4_1 -I$DEEP_HOME/readerWriter -I$DEEP_HOME/scenario -I$DEEP_HOME/readerWriter/OpenSplice/generated -I$DEEP_HOME/vendorAbstraction/OpenSplice -I$OSPL_HOME/include/dcps/C++/SACPP -c $1.cpp -o ../../../obj/OpenSplice/$1.o
